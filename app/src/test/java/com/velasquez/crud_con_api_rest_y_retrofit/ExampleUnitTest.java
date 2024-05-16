e821ee15bc359369e9674d257de6c0e4817a7a52
rvelasquezq <rodrigovelas2@gmail.com>
2024-05-15 21:27:41 -0500

    Primer commit

---
+++ app/src/test/java/com/velasquez/crud_con_api_rest_y_retrofit/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.velasquez.crud_con_api_rest_y_retrofit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
